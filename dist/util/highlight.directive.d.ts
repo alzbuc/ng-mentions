3235f1d0480c70099946905ce48341a5b027889b
Filip Kudla <filip.kudla@hardsun.eu>
2023-08-15 12:42:52 +0200

    remove build

--- dist/util/highlight.directive.d.ts
+++
@@ -1,7 +0,0 @@
-import { Tag } from './interfaces';
-import * as i0 from "@angular/core";
-export declare class HighlightedDirective {
-    tag: Tag;
-    static ɵfac: i0.ɵɵFactoryDeclaration<HighlightedDirective, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<HighlightedDirective, "highlighted", never, { "tag": { "alias": "tag"; "required": false; }; }, {}, never, never, false, never>;
-}
